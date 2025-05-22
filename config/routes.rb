86a8c14a1fd53e0c46aca782c7ef2d37894ed22f
satoshi-yamashita-sit <satoshi.yamashita.se@gmail.com>
2025-05-22 15:58:39 +0900

    ユーザ登録画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/index'
+  root to: 'users#index'
+  get 'users/new', to: 'users#new'
+  get 'users/show'
+  get 'users/edit'
+  post 'users', to: 'users#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

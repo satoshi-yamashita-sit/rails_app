4402ddff5205581cc434bfdb270678e0fc7c99ff
satoshi-yamashita-sit <satoshi.yamashita.se@gmail.com>
2025-05-23 09:32:40 +0900

    編集ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # get 'users/new'
   # get 'users/index'
   # get 'users/show'
+  # get 'users/edit'
   root to: 'users#index'
   get 'users/new', to: 'users#new'
   get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/edit'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
   post 'users', to: 'users#create'
+  patch 'users/:id', to: 'users#update', as: 'update_user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
